9b214181916dd6c23681b4b83093b0176e82711b
ext-nolanruffenach <ext-nolan.ruffenach@swile.co>
2026-01-09 16:42:21 +0100

    Utilisation Docker

--- run-profile.ps1
+++ run-profile.ps1
@@ -3,8 +3,11 @@ param(
     [string]$profiles = 'h2'
 )
 
-$env:JAVA_HOME='C:\Users\nruffenach\Documents\JAVA\jdk-21'
-$env:PATH="$env:PATH;C:\Users\nruffenach\Documents\JAVA\apache-maven-3.9.12\bin"
+$env:JAVA_HOME="$env:USERPROFILE\Documents\JAVA\jdk-21"
+echo "$env:JAVA_HOME"
+$env:PATH="$env:PATH;$env:USERPROFILE\Documents\JAVA\apache-maven-3.9.12\bin"
+echo "$env:PATH"
 $env:SPRING_PROFILES_ACTIVE="jpa,$profiles"
+echo "$env:SPRING_PROFILES_ACTIVE"
 
 mvn.cmd spring-boot:run
