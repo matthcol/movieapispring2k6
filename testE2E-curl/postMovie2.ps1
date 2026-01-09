9a565ec3fe0abe1d59602f78e9ca4e64e1af7452
matthcol <matthias.colin@gmail.com>
2026-01-09 10:08:21 +0100

    fix curl post scripts

--- testE2E-curl/postMovie2.ps1
+++ testE2E-curl/postMovie2.ps1
@@ -1,12 +1,16 @@
+$json = @'
+{
+  \"title\": \"Avatar: The Way of Water\",
+  \"releaseYear\": 2022,
+  \"duration\": 192,
+  \"genres\": [
+    \"Action\", \"Sci-fi\", \"Adventure\"
+  ]
+}
+'@
+
 curl.exe -X 'POST' `
 'http://localhost:8080/api/movie' `
 -H 'accept: */*' `
 -H 'Content-Type: application/json' `
--d '{
-  "title": "Avatar: The Way of Water",
-  "releaseYear": 2022,
-  "duration": 192,
-  "genres": [
-    "Action", "Sci-fi", "Adventure"
-  ]
-}'
+-d $json
